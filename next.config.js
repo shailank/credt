f6efed91055136d4ef7e10a428fda91b7b7a02f8
Shailank <shailank.singh@conneqtcorp.com>
2022-07-28 08:48:26 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-    eslint: { ignoreDuringBuilds: true },
-	//basePath: '/',
-	//assetPrefix: '',
-	
+  eslint: { ignoreDuringBuilds: true },
+  basePath: "/creditcard",
+  //assetPrefix: '',
+  //basePath: '/'+ process.env.BASE_PATH,
+  //assetPrefix: '/'+ process.env.BASE_PATH,
 
-    reactStrictMode: false,
+  reactStrictMode: false,
 
-    styledComponents: true,
-    // images: {
-    //     domains: [
-    //     "localhost",
-    //     "acqui-uat.kotak.internal",
-    //              ],
-    //     },
+  styledComponents: true,
+  // images: {
+  //     domains: [
+  //     "localhost",
+  //     "acqui-uat.kotak.internal",
+  //              ],
+  //     },
 
-        publicRuntimeConfig: {
-            MY_VAR: process.env.REACT_APP_HTTP_PATH,
-            staticFolder: '/static',
-            mySecret: 'secret',
-            
-          },
-          serverRuntimeConfig: {
-            // Will only be available on the server side
-            mySecret: 'secret',
-            secondSecret: process.env.REACT_APP_HTTP_PATH, // Pass through env variables
-          }
-}
+  publicRuntimeConfig: {
+    MY_VAR: process.env.REACT_APP_HTTP_PATH,
+    staticFolder: "/static",
+    mySecret: "secret",
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: "secret",
+    secondSecret: process.env.REACT_APP_HTTP_PATH, // Pass through env variables
+  },
+};
